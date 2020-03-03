b385501cff0cd48c50bf5738ae2769b12a14f33e
kawachi gou <kawachi20018@gmail.com>
2020-03-04 02:18:15 +0900

    add:　right-column

---
+++ resources/views/front_blog/app.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"><!-- Bootstrap本体 -->
+    <link rel="stylesheet" href="{{ asset('/css/blog.css') }}">
+</head>
+
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <h2><a href="{{ route('front_index') }}">私のブログ</a></h2>
+
+            {{--何らかのエラー表示用--}}
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+
+        {{--メインカラム--}}
+        @yield('main')
+        {{--右サブカラム--}}
+        {{--@include ディレクティブで他のテンプレートを読み込むこともできる--}}
+        @include('front_blog.right_column')
+    </div>
+</div>
+</body>
+</html>
